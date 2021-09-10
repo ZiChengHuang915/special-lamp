1a637a658b84e3ab7f2919524b8a3c376c785165
unknown <zichuang127@gmail.com>
2021-09-10 14:51:27 -0400

    testing new csv

--- test.py
+++ test.py
@@ -12,11 +12,14 @@ class GPU:
         self.kijiji_entries = []
         
 class kijiji_entry:
-    def __init__(self, name, price, gpu_name, gpu_revenue_24h):
+    def __init__(self, gpu_name, gpu_revenue_24h, name, price, description, link):
         self.gpu_name = gpu_name
         self.gpu_revenue_24h = gpu_revenue_24h
         self.name = name
         self.price = price
+        self.description = description
+        self.link = link
+        self.roi = float(price) / float(gpu_revenue_24h)
         
 GPUS = []
 kijiji_entries = []
@@ -42,7 +45,7 @@ for index in range(1, len(whattomine_entries)):
     
     name_container = whattomine_entries[index].find("a")
     gpu_name = name_container.text.strip().splitlines()[2].strip()
-    revenue_24h = whattomine_entries[index].find("td", {"class":"text-right table-"}).text.strip()
+    revenue_24h = float(whattomine_entries[index].find("td", {"class":"text-right table-"}).text.strip().replace("$", "").replace(".", "")) / 100
 
     temp_gpu_name = gpu_name.replace(" ", "-")
     link = "https://www.kijiji.ca/b-gta-greater-toronto-area/" + temp_gpu_name + "/k0l1700272?rb=true&dc=true"
@@ -57,22 +60,25 @@ for index in range(1, len(whattomine_entries)):
     
     names = kijiji_page_soup.findAll("a", {"class":"title"})
     prices = kijiji_page_soup.findAll("div", {"class":"price"})
+    descriptions = kijiji_page_soup.findAll("div", {"class":"description"})
     print("has " + str(len(names)))
     for index in range(0, len(names)):
         temp_name = names[index].text.strip().replace("\n", "").replace(",", "").replace("\"", "")
         temp_price = prices[index].text.strip().replace("\n", "").replace(",", "").replace("$", "").replace(".00", "")
+        temp_description = descriptions[index].text.strip().replace(",", "")
+        temp_link = "https://www.kijiji.ca" + names[index]['href']
         if ("Please Contact" not in temp_price and 
             "Free" not in temp_price and
             "Swap / Trade" not in temp_price):
             if len(re.findall('\d+', gpu_name)) > 0:
                 if re.findall('\d+', gpu_name)[0] in temp_name or (len(re.findall('\d+', gpu_name)) > 1 and re.findall('\d+', gpu_name)[1] in temp_name):
-                    kijiji_entries.append(kijiji_entry(temp_name, temp_price, gpu_name, revenue_24h))
+                    kijiji_entries.append(kijiji_entry(gpu_name, revenue_24h, temp_name, temp_price, temp_description, temp_link))
             else:
-                kijiji_entries.append(kijiji_entry(temp_name, temp_price, gpu_name, revenue_24h))
+                kijiji_entries.append(kijiji_entry(gpu_name, revenue_24h, temp_name, temp_price, temp_description, temp_link))
                 
     GPUS[len(GPUS) - 1].kijiji_entries = kijiji_entries
     print(len(kijiji_entries))
-    write_string = gpu_name + ", " + revenue_24h
+    write_string = gpu_name + ", " + str(revenue_24h)
     for index in GPUS[len(GPUS) - 1].kijiji_entries:
         write_string = write_string + ", " + str(index.name).replace('\uff08', "").replace('\uff09', "") + ", " + str(index.price) # replace is for bad unicode
     for index in range(0, MAX_COLUMNS - len(GPUS[len(GPUS) - 1].kijiji_entries) * 2):
@@ -80,13 +86,21 @@ for index in range(1, len(whattomine_entries)):
     write_string = write_string + "\n"
     print(write_string)
     f.write(write_string)
-    sleep(1)
+    sleep(3)
 
 f.write("\n")
+f.write("Link, Revenue 24H, Price, ROI, Title, Description")
+all_entries = []
 for gpu in GPUS:
     for entry in gpu.kijiji_entries:
-        write_string = entry.gpu_name + "," + entry.gpu_revenue_24h + "," + entry.name.replace('\uff08', "").replace('\uff09', "") + "," + entry.price + "\n"
-        f.write(write_string)
+        all_entries.append(entry)
+        #write_string = entry.link + "," + str(entry.gpu_revenue_24h) + "," + entry.price + "," + str(entry.roi) + "," + entry.name.replace('\uff08', "").replace('\uff09', "")  + "\n"
+        #f.write(write_string)
+
+all_entries.sort(key=lambda x: x.roi)
+for entry in all_entries:
+    write_string = entry.link + "," + str(entry.gpu_revenue_24h) + "," + entry.price + "," + str(entry.roi) + "," + entry.name.replace('\uff08', "").replace('\uff09', "") + "," + entry.description.replace('\uff08', "").replace('\uff09', "").replace('\uff0c', ".").replace('\u202f', "") + "\n"
+    f.write(write_string)
 
 f.close()
 
