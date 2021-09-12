fafa1d5293f8d95f5cf9ebe7d904003f80aa3434
unknown <zichuang127@gmail.com>
2021-09-11 20:49:00 -0400

    fixed bad import

--- testapi.py
+++ testapi.py
@@ -1,16 +1,17 @@
 from flask import Flask,jsonify
 app = Flask(__name__)
 
-from searchKijiji import getKijijiEntries
+from searchKijiji import getKijijiEntries as skGetKijijiEntries
+from searchKijiji import getGPUs as skGetGPUs
 
 @app.route("/getGPUs/", methods=['GET'])
 def getGPUs():
-    names = getGPUs()
+    names = skGetGPUs()
     return jsonify({"gpu_name" : names})
 
-@app.route("/getKijijiDeal/<gpu_name>", methods=['GET'])
-def getKijijiDeal(gpu_name):
-    name,price,description = getKijijiEntries(gpu_name)
+@app.route("/getKijijiEntries/<gpu_name>", methods=['GET'])
+def getKijijiEntries(gpu_name):
+    name,price,description = skGetKijijiEntries(gpu_name)
     return jsonify({"gpu_name" : name, "price" : price, "desc" : description})
 
 if __name__ == "__main__":
